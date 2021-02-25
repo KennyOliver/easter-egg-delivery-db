2c84bc09b92f9a595c2ba3c315ecc4b0c3be1769
Kenny <kenny_oliver@icloud.com>
2021-02-25 20:31:52 +0000

    [+] Added shipping cost data to CSVs [+] Added support for shipping cost to be printed

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ def scrape_csv(chosen_file):
     egg_type = record[1]
     size = record[2]
     price = record[3]
-    quantity = record[4]
+    shipping = record[4]
+    quantity = record[5]
     #print(record)
-    print(f"{manufacturer},{egg_type},{size},£{price},{quantity}")
+    print(f"{manufacturer},{egg_type},{size},£{price},£{shipping},{quantity}")
     line_count += 1
   print("-" * 30)
   print(f"{line_count} results found!")
@@ -25,3 +26,8 @@ scrape_csv("order1")
 
 #========NOTE========
 """ Use python -c 'function(argument)' to run in Bash cmd line """
+
+""" Smiths: small = 0.60 medium = 0.70 large = 0.80
+Amsters: small = 0.40 medium = 0.50 large = 0.60
+E&S: small = 0.90 medium = 1.00 large = 1.10
+Nesters: small = 0.90 medium = 0.95 large = 0.99 """
