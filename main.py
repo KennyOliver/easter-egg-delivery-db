1dcd69542a239af43fc27f8a752f40bb843266e6
Kenny <kenny_oliver@icloud.com>
2021-03-04 14:35:02 +0000

    LOTS of under-the-hood changes!
    [+] Now printing results stats using separate procedure
    [+] Added header to every CSV, but skipping them to prevent errors
    [+] Renamed manufacturer variable to brand
    [+] Removed lots of unnecessary comments
    [+] Really shortened the if statement in menu()
    [+] Print available files using for loop, rather than lots of \n and \t

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ import csv
 def scrape_csv(chosen_file,search_query):
   print(f"<-- {chosen_file}.csv -->")
   
-  current_csv = open(rf"{chosen_file}.csv",'r') #open using filename passed through function
+  current_csv = open(rf"{chosen_file}.csv",'r') #open filename passed through function
   reader = csv.reader(current_csv)
   
-  record_count = 0 #counts number of records
+  record_count = 0
   total_eggs = 0
   total_cost = 0
   total_shipping = 0
   
+  next(reader,None) #skips header line
   for record in reader:
-    manufacturer = record[0]
+    brand = record[0]
     egg_type = record[1]
     size = record[2]
     price = record[3]
@@ -20,91 +21,70 @@ def scrape_csv(chosen_file,search_query):
     quantity = record[5]
     
     if search_query in record:
-      print(f"{manufacturer},{egg_type},{size},£{price},£{shipping},{quantity}") #print each record with same appearance as a CSV file
+      print(f"{brand},{egg_type},{size},£{price},£{shipping},{quantity}")
       record_count += 1
       total_eggs += int(quantity)
       total_cost += float(price)
       total_shipping += float(shipping)
   
   print("-" * 30) #UI visual divider
-  print(f"Using \"*\" wildcard...")
-  print(f"\tResults: {record_count}")
-  print(f"\tTotal eggs ordered: {total_eggs}")
-  print(f"\tEggs cost: £{total_cost}")
-  print(f"\tCost with shipping: £{total_cost + total_shipping}")
-  avg_egg_cost = (total_cost) / total_eggs
-  print(f"\tAvg cost per egg (no shipping): £{round(avg_egg_cost,3)}")
+  print(f"Using \"{search_query}\" query...")
+  stats(record_count,total_eggs,total_cost,total_shipping)
   
   current_csv.close()
 #====================
 def all_in_csv(chosen_file):
   print(f"<-- {chosen_file}.csv -->")
   
-  current_csv = open(rf"{chosen_file}.csv",'r') #open using filename passed through function
+  current_csv = open(rf"{chosen_file}.csv",'r') #open filename passed through function
   reader = csv.reader(current_csv)
   
-  record_count = 0 #counts number of records
+  record_count = 0
   total_eggs = 0
   total_cost = 0
   total_shipping = 0
   
-  for record in reader:
-    manufacturer = record[0]
-    egg_type = record[1]
-    size = record[2]
-    price = record[3]
-    shipping = record[4]
-    quantity = record[5]
-    
-    print(f"{manufacturer},{egg_type},{size},£{price},£{shipping},{quantity}") #print each record with same appearance as a CSV file
+  next(reader,None) #skips header line
+  for brand,egg_type,size,price,shipping,quantity in reader:
+    print(f"{brand},{egg_type},{size},£{price},£{shipping},{quantity}")
     record_count += 1
     total_eggs += int(quantity)
     total_cost += float(price)
     total_shipping += float(shipping)
   
   print("-" * 30) #UI visual divider
-  print(f"Using \"*\" wildcard...")
-  print(f"\tResults: {record_count}")
-  print(f"\tTotal eggs ordered: {total_eggs}")
-  print(f"\tEggs cost: £{total_cost}")
-  print(f"\tCost with shipping: £{total_cost + total_shipping}")
-  avg_egg_cost = (total_cost) / total_eggs
-  print(f"\tAvg cost per egg (no shipping): £{round(avg_egg_cost,3)}")
+  print("Using \"*\" wildcard...")
+  stats(record_count,total_eggs,total_cost,total_shipping)
   
   current_csv.close()
 #====================
+def stats(count,eggs,cost,shipping):
+  print(f"{count} results")
+  print(f"\tTotal eggs ordered: {eggs}")
+  print(f"\tEggs cost: £{cost}")
+  print(f"\tCost with shipping: £{cost + shipping}")
+  avg_egg_cost = (cost) / shipping
+  print(f"\tAvg cost per egg (no shipping): £{round(avg_egg_cost,3)}")
+#====================
 def menu():
   print("Choose the desired file to scrape:")
-  print("\t[1] order1.csv\n\t[2] order2.csv\n\t[3] order3.csv\n\t[4] order4.csv")
-  user_pick_file = input("\t--> ")
-  while user_pick_file not in ['1','2','3','4'] or len(user_pick_file) != 1:
-    user_pick_file = input("\t--> ")
+  for i in range(1,4+1):
+    print(f"\t[{i}] order{i}.csv")
+  file_choice = input("\t--> ")
+  while file_choice not in ['1','2','3','4'] or len(file_choice) != 1:
+    file_choice = input("\t--> ")
   
-  user_query = input("Enter query\n\t[*] To print all\n\t--> ")
+  print("Enter query\n\t[*] To print all")
+  user_query = input("\t--> ")
   while len(user_query) < 1:
     user_query = input("\t--> ")
   
   print("-" * 30)
-  if user_pick_file == '1':
-    if user_query != '*':
-      scrape_csv("order1",user_query)
-    else:
-      all_in_csv("order1")
-  elif user_pick_file == '2':
-    if user_query != '*':
-      scrape_csv("order2",user_query)
-    else:
-      all_in_csv("order2")
-  elif user_pick_file == '3':
-    if user_query != '*':
-      scrape_csv("order3",user_query)
-    else:
-      all_in_csv("order3")
-  elif user_pick_file == '4':
-    if user_query != '*':
-      scrape_csv("order4",user_query)
-    else:
-      all_in_csv("order4")
+  
+  if user_query != "*":
+    scrape_csv(f"order{file_choice}",user_query)
+  elif user_query == "*":
+    all_in_csv(f"order{file_choice}")
 #====================
 # MAIN PROGRAM
 print("<-- Easter Egg Delivery DB -->")
